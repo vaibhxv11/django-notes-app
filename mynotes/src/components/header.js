6910643fa31d14f3081f11c14ddfefca6900947e
Vaibhav D <97098598+vaibhxv11@users.noreply.github.com>
2025-03-19 11:41:03 +0530

    Update header.js

--- mynotes/src/components/header.js
+++ mynotes/src/components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="app-header">
-            <h1>My Notes By Vaibhav Dudhal</h1>
+            <h1>My Notes By Vaibhav </h1>
         </div>
     )
 }
